4a1879e3d7fa811ecc9c2d65da40f0a379dee7ac
arielhenryson <ariel@codefresh.io>
2019-01-31 11:40:54 +0200

    0.0.1-alpha3

--- src/cfReceiver.service.ts
+++ src/cfReceiver.service.ts
@@ -9,41 +9,67 @@ export class CfReceiverService {
     config: Config
 
 
-    constructor(config: Config) {
+    constructor(
+        config: Config
+    ) {
         this.config = config
         console.log('starting CfReceiverService')
     }
 
+
     /**
      * open connection to the WebSocket end point
      */
-    init() {
+    watch() {
         return new Observable(obser => {
-            this.ws = new WebSocket(`ws://${this.config.endPoint}/${this.config.workflowID}`)
+            console.log(`watch for data from  ws://${this.config.endPoint}/${this.config.workflowID}`)
 
-
-            this.ws.onerror = (event: any) => {
-                obser.next({
-                    type: 'error',
-                    event
-                })
-            }
-
-
-            this.ws.onclose = (event: any) => {
-                obser.next({
-                    type: 'close',
-                    event
-                })
-            }
-
-
-            this.ws.onmessage = (event: any) => {
-                obser.next({
-                    type: 'message',
-                    event
-                })
-            }
+            this.connect(obser)
         })
+    }
+
+
+
+    connect(obser) {
+        console.log('connecting to WebSocket')
+
+        this.ws = new WebSocket(`ws://${this.config.endPoint}/${this.config.workflowID}`)
+
+
+        this.ws.onerror = (event: any) => {
+            obser.next({
+                type: 'error',
+                event
+            })
+        }
+
+
+        this.ws.onclose = (event: any) => {
+            obser.next({
+                type: 'close',
+                event
+            })
+
+
+            this.retry(obser)
+        }
+
+
+        this.ws.onmessage = (event: any) => {
+            obser.next({
+                type: 'message',
+                event
+            })
+        }
+    }
+
+
+    retry(obser) {
+        console.log('try to reconnect to WebSocket')
+
+
+        setTimeout(() => {
+            this.connect(obser)
+        }, this.config.reconnectDelay)
     }
 }
--- src/interfaces/config.interface.ts
+++ src/interfaces/config.interface.ts
@@ -1,4 +1,5 @@
 export interface Config {
     endPoint: string
     workflowID: string
+    reconnectDelay: number
 }
